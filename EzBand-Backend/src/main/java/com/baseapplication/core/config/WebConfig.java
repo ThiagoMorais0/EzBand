c91ea04ea82c833bbc332422486750fc1f59a17a
Thiago Morais <thiagomface@gmail.com>
2026-06-07 14:57:24 -0300

    Correção

--- EzBand-Backend/src/main/java/com/baseapplication/core/config/WebConfig.java
+++ EzBand-Backend/src/main/java/com/baseapplication/core/config/WebConfig.java
@@ -2,18 +2,10 @@ package com.baseapplication.core.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS");
-    }
 
     @Override
     public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
